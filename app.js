18043a411bb7afb93eaa297c45071f4c34d7595f
ajdin <ajdinhusetovic@outlook.com>
2023-01-12 20:33:21 +0100

    styled the add project button and the input

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ addTodoButton.addEventListener('click', () => {
                 todo.innerHTML = `${todo.textContent} <i class="fa-regular fa-trash-can"></i>`
                 todo.classList.add('todo');
                 todoContainer.appendChild(todo);
+                // shows button, hides input
                 addTodoButton.hidden = false;
                 todoInput.hidden = true;
             }

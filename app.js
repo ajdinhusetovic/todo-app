a5d999d5099afb97f76d9cd348c4f475bce23c5e
ajdin <ajdinhusetovic@outlook.com>
2023-01-12 20:27:26 +0100

    addProjectButton hides when clicked, input shows and after input is submitted input hides and the addProjectButton shows again

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const addTodoButton = document.querySelector('#add-todo');
 const todoContainer = document.querySelector('.todo-container');
 const leftSide = document.querySelector('.left-side');
 
-function hide(){
-    addTodoButton.style.visibility = "visible";
-}
 
+// event listener for add todo button
 addTodoButton.addEventListener('click', () => {
+    // hides the button, input shows
     addTodoButton.hidden = true;
     let todoInput = document.createElement('input');
     leftSide.appendChild(todoInput);
+    // listens for enter key in input field
     todoInput.addEventListener('keyup', (e) => {
         if (todoInput.value === "") {
             alert("Please enter your todo");
@@ -20,9 +20,11 @@ addTodoButton.addEventListener('click', () => {
                 todo.innerHTML = `${todo.textContent} <i class="fa-regular fa-trash-can"></i>`
                 todo.classList.add('todo');
                 todoContainer.appendChild(todo);
+                addTodoButton.hidden = false;
+                todoInput.hidden = true;
             }
         }
-    })
+    });
 });
 
 
